ef35cff5a7c4ab1e2a42f090a641412ccf12086a
David Herzka <david@osdyne.com>
2023-05-11 13:39:07 -0400

    Make bitenum support variants that cover ranges of values

--- bitbybit/src/bitenum.rs
+++ bitbybit/src/bitenum.rs
@@ -1,10 +1,42 @@
 use proc_macro::TokenStream;
+use syn::punctuated::Punctuated;
+use syn::token::Comma;
+use std::fmt::LowerHex;
+use std::ops::RangeInclusive;
 use std::str::FromStr;
 
-use proc_macro2::TokenTree;
-use quote::{quote, ToTokens};
+use proc_macro2::{TokenTree};
+use quote::{quote, ToTokens, TokenStreamExt};
 use syn::__private::TokenStream2;
-use syn::{Attribute, Data, DeriveInput, Expr, Ident};
+use syn::{Attribute, Data, DeriveInput, Expr, Ident, ExprRange, Variant, LitInt, Lit, RangeLimits};
+
+struct BitenumVariant<'a> {
+    variant: &'a Variant,
+}
+
+const CUSTOM_VARIANT_ATTRIBUTES: [&'static str; 1] = [
+    "range",
+];
+
+impl ToTokens for BitenumVariant<'_> {
+    fn to_tokens(&self, tokens: &mut proc_macro2::TokenStream) {
+        // Remove custom attributes, but still emit any we don't recognize
+        let non_elided_attrs = self.variant.attrs.iter().filter(|attr| {
+            if let Some(ident) = attr.path().get_ident() {
+                !CUSTOM_VARIANT_ATTRIBUTES.contains(&ident.to_string().as_str())
+            } else {
+                true
+            }
+        });
+        tokens.append_all(non_elided_attrs);
+        self.variant.ident.to_tokens(tokens);
+        self.variant.fields.to_tokens(tokens);
+        if let Some((eq_token, disc)) = &self.variant.discriminant {
+            eq_token.to_tokens(tokens);
+            disc.to_tokens(tokens);
+        }
+    }
+}
 
 pub fn bitenum(args: TokenStream, input: TokenStream) -> TokenStream {
     let args: Vec<_> = proc_macro2::TokenStream::from(args).into_iter().collect();
@@ -140,42 +172,103 @@ pub fn bitenum(args: TokenStream, input: TokenStream) -> TokenStream {
         _ => panic!("bitenum!: Must be used on enum"),
     };
     let mut uses_conditional = false;
-    let emitted_variants: Vec<(&Expr, u128, &Ident, Vec<Attribute>)> = variants.iter().map(|variant| {
+
+    enum VariantValue<'a> {
+        SingleValue(&'a Expr, u128),
+        Range(ExprRange, RangeInclusive<u128>),
+    }
+
+    let max_value = (1u128 << bit_count) - 1;
+
+    let emitted_variants: Vec<(VariantValue, &Ident, Vec<Attribute>)> = variants.iter().map(|variant| {
         let variant_name = &variant.ident;
-        let discriminant = variant.discriminant.as_ref().unwrap_or_else(|| panic!("bitenum!: Variant '{}' needs to have a value", variant_name));
-        // Discriminant.0 is the equals sign. 1 is the value
-        let value = &discriminant.1;
-        let string_value = value.to_token_stream().to_string().replace('_', "");
+        let value = {
+            let range_attrs: Vec<&Attribute> = variant.attrs.iter().filter(|attr| {
+                attr.path().is_ident("range")
+            }).collect();
+            if range_attrs.len() > 1 {
+                panic!("bitenum!: multiple range attributes for {variant_name}");
+            }
 
-        // Determine the integer value itself. While we don't need it further down (for now),
-        // this ensures that only constants are being used; due to the way how new_with_raw_value()
-        // is written, some expressions would cause compilation issues (e.g. those that refer to other
-        // enum values).
-        let int_value = if let Some(stripped) = string_value.strip_prefix("0x") {
-            u128::from_str_radix(stripped, 16)
-        } else if let Some(stripped) = string_value.strip_prefix("0b") {
-            u128::from_str_radix(stripped, 2)
-        } else if let Some(stripped) = string_value.strip_prefix("0o") {
-            u128::from_str_radix(stripped, 8)
-        } else {
-            string_value.parse::<u128>()
-        }.unwrap_or_else(|_| panic!("bitenum!: Error parsing '{}' as integer. Supported: hexadecimal, octal, binary and decimal unsigned integers, but not expressions", string_value));
+            if let Some(range_attr) = range_attrs.first() {
+                let attr_args: Expr = range_attr.parse_args().expect("bitenum: failed to parse range attribute");
+                let expr_range = match attr_args {
+                    Expr::Range(e) => e,
+                    _ => panic!("bitenum!: range attribute argument must be a range"),
+                };
 
-        if int_value >= (1u128 << bit_count) {
-            panic!("bitenum!: Value {} exceeds the given number of bits", variant_name);
-        }
+                let start = match expr_range.clone().start.expect("bitenum: range must have start and end bounds").as_ref() {
+                    Expr::Lit(expr_lit) => match &expr_lit.lit {
+                        Lit::Int(lit_int) => {
+                            lit_int.base10_parse::<u128>().expect("bitenum: failed to parse range start")
+                        },
+                        _ => panic!("bitenum!: invalid range start"),
+                    },
+                    _ => panic!("bitenum!: invalid range start"),
+                };
+                let end = match expr_range.clone().end.expect("bitenum: range must have start and end bounds").as_ref() {
+                    Expr::Lit(expr_lit) => match &expr_lit.lit {
+                        Lit::Int(lit_int) => {
+                            lit_int.base10_parse::<u128>().expect("bitenum: failed to parse range end")
+                        },
+                        _ => panic!("bitenum!: invalid range start"),
+                    },
+                    _ => panic!("bitenum!: invalid range end"),
+                };
+                match expr_range.clone().limits {
+                    RangeLimits::HalfOpen(_) => panic!("bitenum!: range must be closed"),
+                    _ => (),
+                };
+
+                if start > max_value {
+                    panic!("bitenum!: range start for {variant_name} exceeds the given number of bits")
+                }
+
+                if end > max_value {
+                    panic!("bitenum!: range end for {variant_name} exceeds the given number of bits")
+                }
+
+                VariantValue::Range(expr_range.clone(), start..=end)
+            } else {
+                let discriminant = variant.discriminant.as_ref().unwrap_or_else(|| panic!("bitenum!: Variant '{}' needs to have a value", variant_name));
+                // Discriminant.0 is the equals sign. 1 is the value
+                let value = &discriminant.1;
+                let string_value = value.to_token_stream().to_string().replace('_', "");
+
+                // Determine the integer value itself. While we don't need it further down (for now),
+                // this ensures that only constants are being used; due to the way how new_with_raw_value()
+                // is written, some expressions would cause compilation issues (e.g. those that refer to other
+                // enum values).
+                let int_value = if let Some(stripped) = string_value.strip_prefix("0x") {
+                    u128::from_str_radix(stripped, 16)
+                } else if let Some(stripped) = string_value.strip_prefix("0b") {
+                    u128::from_str_radix(stripped, 2)
+                } else if let Some(stripped) = string_value.strip_prefix("0o") {
+                    u128::from_str_radix(stripped, 8)
+                } else {
+                    string_value.parse::<u128>()
+                }.unwrap_or_else(|_| panic!("bitenum!: Error parsing '{}' as integer. Supported: hexadecimal, octal, binary and decimal unsigned integers, but not expressions", string_value));
+
+                if int_value > max_value {
+                    panic!("bitenum!: Value {} exceeds the given number of bits", variant_name);
+                }
+
+                VariantValue::SingleValue(value, int_value)
+            }
+        };
 
         let mut cfg_attributes = Vec::new();
 
         for attr in &variant.attrs {
-            if attr.path().to_token_stream().to_string() == "cfg" {
+            if attr.path().is_ident("cfg") {
                 cfg_attributes.push(attr.clone());
                 uses_conditional = true;
             }
         }
 
-        (value, int_value, variant_name, cfg_attributes)
+        (value, variant_name, cfg_attributes)
     }).collect();
+
 
     if uses_conditional {
         if exhaustiveness != Exhaustiveness::Conditional {
@@ -190,16 +283,46 @@ pub fn bitenum(args: TokenStream, input: TokenStream) -> TokenStream {
     // We tested the numeric values for out-of-bounds above. As enum values are unique integers,
     // we can now reason about the number of variants: If variants == 2^bits then we have to be exhaustive
     // (and if not, we can't be).
-    let possible_maximum_variants = 1u128 << bit_count;
+
+    let mut ranges: Vec<RangeInclusive<u128>> = Vec::new();
+    for (value, _, _) in &emitted_variants {
+        match value {
+            VariantValue::SingleValue(_, v) => ranges.push(*v..=*v),
+            VariantValue::Range(_, range) => ranges.push(range.clone()),
+        }
+    }
+    ranges.sort_by_key(|r| *r.start());
+
+    let mut has_holes = false;
+    let mut last_end: Option<u128> = None;
+    for range in ranges {
+        if let Some(last_end) = last_end {
+            if *range.start() != last_end + 1 {
+                has_holes = true;
+            }
+            if *range.start() <= last_end {
+                panic!("bitenum!: one or more value is covered by multiple variants ({:02x}-{:02x})", range.start(), last_end);
+            }
+        } else {
+            has_holes = *range.start() > 0;
+        }
+        last_end = Some(*range.end());
+    }
+
+    let covers_all_values = match last_end {
+        Some(last_end) => !has_holes && last_end == max_value,
+        None => false,
+    };
+
     let return_is_result = match exhaustiveness {
         Exhaustiveness::True => {
-            if emitted_variants.len() != possible_maximum_variants as usize {
+            if !covers_all_values {
                 panic!("bitenum!: Enum is marked as exhaustive, but it is missing variants")
             }
             false
         }
         Exhaustiveness::False => {
-            if emitted_variants.len() == possible_maximum_variants as usize {
+            if covers_all_values {
                 panic!("bitenum!: Enum is exhaustive, but not marked accordingly. Add 'exhaustive: true'")
             }
             true
@@ -219,16 +342,20 @@ pub fn bitenum(args: TokenStream, input: TokenStream) -> TokenStream {
 
     let case_values: Vec<TokenStream2> = emitted_variants
         .iter()
-        .map(|(expression, _int_value, name, cfg_attributes)| {
+        .map(|(value, name, cfg_attributes)| {
+            let (case_expression, case_value_expression) = match value {
+                VariantValue::SingleValue(expression, _) => (expression.to_token_stream(), quote! { Self::#name }),
+                VariantValue::Range(expression, _) => (expression.to_token_stream(), quote! { Self::#name(value) }),
+            };
             if return_is_result {
                 quote! {
                     #( #cfg_attributes )*
-                    #expression => Ok(Self::#name),
+                    #case_expression => Ok(#case_value_expression),
                 }
             } else {
                 quote! {
                     #( #cfg_attributes )*
-                    #expression => Self::#name,
+                    #case_expression => #case_value_expression,
                 }
             }
         })
@@ -260,16 +387,46 @@ pub fn bitenum(args: TokenStream, input: TokenStream) -> TokenStream {
         )
     };
 
+    let wrapped_variants = variants
+        .iter()
+        .map(|variant| BitenumVariant{ variant })
+        .collect::<Punctuated<BitenumVariant, Comma>>();
+
+    let raw_value_function = {
+        let raw_value_cases: Punctuated<TokenStream2, Comma> = emitted_variants.iter().map(|(value, name, cfg_attributes)| {
+            match value {
+                VariantValue::SingleValue(expression, _) => quote! {
+                    #( #cfg_attributes )*
+                    Self::#name => #result_constructor(#expression)
+                },
+                VariantValue::Range(_, _) => quote! {
+                    #( #cfg_attributes )*
+                    Self::#name(v) => v
+                }
+            }
+        })
+        .collect();
+
+        quote! {
+            /// Returns the underlying raw value of this bitfield
+            pub const fn raw_value(self) -> #bounded_data_type {
+                match self {
+                    #raw_value_cases
+                }
+            }
+        }
+    };
+
     let expanded = quote! {
+        #[repr(#base_data_type)]
         #[derive(Copy, Clone)]
         #( #enum_attrs )*
         #enum_vis enum #enum_name {
-            #variants
+            #wrapped_variants
         }
 
         impl #enum_name {
-            /// Returns the underlying raw value of this bitfield
-            pub const fn raw_value(self) -> #bounded_data_type { #result_constructor(self as #base_data_type) }
+            #raw_value_function
 
             #constructor_function
         }
