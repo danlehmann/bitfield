5deec417b66d2975e567f3e01d350a9f068bb0f4
Esteban Küber <esteban@osdyne.com>
2026-02-12 18:56:18 -0800

    Add tests for overlapping non-contiguous bit ranges

--- bitbybit/tests/no_compile/invalid_field_range.rs
+++ bitbybit/tests/no_compile/invalid_field_range.rs
@@ -1,3 +1,4 @@
+use arbitrary_int::u4;
 use bitbybit::bitfield;
 
 #[bitfield(u32, default = 0)]
@@ -6,4 +7,27 @@ struct Test {
     invalid_range: u8,
 }
 
-fn main() {}
+#[bitfield(u8)]
+struct Test {
+    #[bits([0, 2, 4, 6], rw)]
+    a: u4,
+    #[bits([1, 3, 5, 7], rw)]
+    b: u4,
+    #[bit(4, rw)]
+    c: bool,
+}
+
+#[bitfield(u8)]
+struct Test2 {
+    #[bits([0, 2, 4, 6], rw)]
+    a: u4,
+    #[bits([1, 2, 5, 7], rw)]
+    b: u4,
+}
+
+
+fn main() {
+    Test::builder().with_a(u4::new(1)).with_b(u4::new(1)).with_c(true).build();
+    Test::builder().with_c(true).build();
+    Test2::builder().with_a(u4::new(1)).with_b(u4::new(1)).build();
+}
